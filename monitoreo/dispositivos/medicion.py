7a34707bfb0a871d901ed1b2124a0f8232f092be
Zaoryk <noiviryn@gmail.com>
2025-08-27 12:23:46 -0400

    Creacion modelo alerta categoria y zona

--- monitoreo/dispositivos/medicion.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Medicion(models.Model):
-    dispositivo = models.ForeignKey(Dispositivo, on_delete=models.CASCADE, related_name="mediciones")
-    fecha_hora = models.DateTimeField(auto_now_add=True)
-    consumo_actual = models.DecimalField(max_digits=10, decimal_places=2)
-
-    def __str__(self):
-        return f"{self.dispositivo.nombre} - {self.consumo_actual}W - {self.fecha_hora}"
