16a322e0bcc2cf02435ecdf357a4f9e867aa0aa9
Zaoryk <noiviryn@gmail.com>
2025-08-27 12:21:05 -0400

    Modelos Dispositivos y Medicion creados

--- monitoreo/dispositivos/models.py
+++ monitoreo/dispositivos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+from .categoria import Categoria
+from .zona import Zona
+from .dispositivo import Dispositivo
+from .medicion import Medicion
+from .alerta import Alerta
+
--- monitoreo/dispositivos/views.py
+++ monitoreo/dispositivos/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def inicio(request):
+    return HttpResponse("messi")
+
+def panel_dispositivos(request):
+    dispositivos = [
+        {"nombre": "Sensor Temperatura", "consumo": 100 },
+        {"nombre": "Medidor Solar", "consumo": 120},
+        {"nombre": "Sensor Movimiento", "consumo": 30},
+        {"nombre": "Calefactor", "consumo": 200},
+    ]
+
+    consumo_maximo = 100
+
+    return render(request, "dispositivos/panel.html", {
+        "dispositivos": dispositivos,
+        "consumo_maximo": consumo_maximo
+    })
