63fd6beb7fcc0fdcb6adcefec78f92fd4e44b553
Zaoryk <noiviryn@gmail.com>
2025-08-27 12:28:46 -0400

    BaseModel

--- monitoreo/dispositivos/models.py
+++ monitoreo/dispositivos/models.py
@@ -6,3 +6,14 @@ from .dispositivo import Dispositivo
 from .medicion import Medicion
 from .alerta import Alerta
 
+class BaseModel(models.Model):
+    ESTADOS = [
+        ("ACTIVO","Activo"),("INACTIVO","Inactivo"),
+    ]
+    estado = models.CharField(max_length=10,choices = ESTADOS, default = "ACTIVO")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        abstract = True
